1a60f79d53db8647fd72344b5c5568a0befc9812
thebowski <anonymaus@outlook.com>
2018-05-18 05:39:32 +0000

    Only reply to comments once

--- jbp_bot.py
+++ jbp_bot.py
@@ -2,7 +2,8 @@
 import logging
 import signal
 import random
-from typing import List, Optional, Callable, Dict, Tuple
+import pickle
+from typing import Deque, List, Optional, Callable, Dict, Tuple
 
 import praw
 from slack_python_logging import slack_logger
@@ -10,7 +11,7 @@ from slack_python_logging import slack_logger
 
 class JbpBot(object):
     """Replies to users with a Petersonian response"""
-    __slots__ = ["reddit", "subreddit", "config", "logger",
+    __slots__ = ["reddit", "subreddit", "config", "logger", "parsed",
                  "villians", "verbs", "favorites", "weapons", "conclusions"]
 
     def __init__(self, reddit: praw.Reddit, subreddit: str) -> None:
@@ -25,6 +26,7 @@ class JbpBot(object):
         self.reddit: praw.Reddit = reddit
         self.subreddit: praw.models.Subreddit = self.reddit.subreddit(
             subreddit)
+        self.parsed: Deque[str] = self.load()
         register_signals()
         self._create_lists()
         random.seed()
@@ -34,6 +36,7 @@ class JbpBot(object):
         """defines exit function"""
         import os
         _ = frame
+        self.save()
         self.logger.info("Exited gracefully with signal %s", signum)
         os._exit(os.EX_OK)
         return
@@ -46,8 +49,11 @@ class JbpBot(object):
             for comment in self.subreddit.stream.comments(pause_after=1):
                 if comment is None:
                     break
-                self.handle_comment(comment)
-            return
+                if str(comment) in self.parsed:
+                    self.logger.debug(f"Comment {str(comment)} found in parsed, skip")
+                    continue
+                else:
+                    self.handle_comment(comment)
         except prawcore.exceptions.ServerError:
             self.logger.error("Server error: Sleeping for 1 minute.")
             sleep(60)
@@ -61,6 +67,7 @@ class JbpBot(object):
     def handle_comment(self, comment: praw.models.Comment) -> None:
         """handles parsing and reply"""
         split: List[str] = comment.body.upper().split()
+        self.parsed.append(str(comment))
 
         if "!JBP" in split:
             self.logger.debug("JBP request found in %s", str(comment))
@@ -77,7 +84,49 @@ class JbpBot(object):
         conclusion: str = random.choice(self.conclusions)
         response:   str = f"The wise man bowed his head solemnly and spoke: \"{villian} {verb} {favorite} because of their {weapon} {conclusion}\""
         return response
+    def load(self) -> Deque[str]:
+        """loads pickle if it exists"""
+        self.logger.debug("Loading pickle file")
+        try:
+            with open("jbp_parsed.pkl", 'rb') as parsed_file:
+                try:
+                    parsed: Deque[str] = pickle.loads(parsed_file.read())
+                    self.logger.debug("Loaded pickle file")
+                    self.logger.debug("Current Size: %s", len(parsed))
+                    if parsed.maxlen != 10000:
+                        self.logger.warning(
+                            "Deque length is not 10000, returning new one")
+                        return Deque(parsed, maxlen=10000)
+                    self.logger.debug("Maximum Size: %s", parsed.maxlen)
+                    return parsed
+                except EOFError:
+                    self.logger.debug("Empty file, returning empty deque")
+                    return Deque(maxlen=10000)
+        except FileNotFoundError:
+            self.logger.debug("No file found, returning empty deque")
+            return Deque(maxlen=10000)
 
+    def load(self) -> Deque[str]:
+        """loads pickle if it exists"""
+        self.logger.debug("Loading pickle file")
+        try:
+            with open("jbp_parsed.pkl", 'rb') as parsed_file:
+                try:
+                    parsed: Deque[str] = pickle.loads(parsed_file.read())
+                    self.logger.debug("Loaded pickle file")
+                    self.logger.debug("Current Size: %s", len(parsed))
+                    if parsed.maxlen != 10000:
+                        self.logger.warning(
+                            "Deque length is not 10000, returning new one")
+                        return Deque(parsed, maxlen=10000)
+                    self.logger.debug("Maximum Size: %s", parsed.maxlen)
+                    return parsed
+                except EOFError:
+                    self.logger.debug("Empty file, returning empty deque")
+                    return Deque(maxlen=10000)
+        except FileNotFoundError:
+            self.logger.debug("No file found, returning empty deque")
+            return Deque(maxlen=10000)
 
     def _create_lists(self):
         self.villians: List[str] = [
@@ -121,7 +170,7 @@ class JbpBot(object):
             "the humble lobster's quest"]
         self.weapons: List[str] = [
             "murderous equity doctrine",
-            "dangerous egalitarian utopia",
+            "dangerous dreams of egalitarian utopia",
             "hatred of Objective truth",
             "compelled speech",
             "group identity",
