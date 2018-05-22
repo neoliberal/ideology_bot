e70ce6ced673eaab30730a05d7934222c5c77b06
Abhi Agarwal <goldenercobra@gmail.com>
2018-05-22 17:40:54 -0400

    get right keys

--- service.py
+++ service.py
@@ -13,11 +13,10 @@ def main() -> None:
     """main service function"""
 
     reddit: praw.Reddit = praw.Reddit(
-        client_id=os.environ["jbp_client_id"],
-        client_secret=os.environ["jbp_client_secret"],
-        refresh_token=os.environ["jbp_refresh_token"],
-        user_agent="linux:jbp_bot:v1.0 (by /r/Neoliberal)"
-    )
+        client_id=os.environ["ideology_bot_client_id"],
+        client_secret=os.environ["ideology_bot_client_secret"],
+        refresh_token=os.environ["ideology_bot_refresh_token"],
+        user_agent="linux:jbp_bot:v1.0 (by /r/Neoliberal)")
 
     bot: IdeologyBot = IdeologyBot(
         reddit
